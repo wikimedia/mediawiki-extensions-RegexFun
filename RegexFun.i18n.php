fc448d11956365ee35cf8e63086cf8d833899d7e
Raimond Spekking <raymond@users.mediawiki.org>
2012-04-20 19:05:47 +0000

    Localisation updates from http://translatewiki.net.

--- RegexFun.i18n.php
+++ RegexFun.i18n.php
@@ -129,11 +129,12 @@ $messages['ia'] = array(
 
 /** Japanese (日本語)
  * @author Fryed-peach
+ * @author Shirayuki
  */
 $messages['ja'] = array(
 	'regexfun-desc' => 'ウィキページ内で正規表現の使用を可能にするパーサー関数を追加する',
 	'regexfun-invalid' => '正規表現「$1」は不正です。',
-	'regexfun-limit-exceed' => 'Regex Fun の正規表現処理最大数 {{PLURAL:$1|$1}} に達しました。',
+	'regexfun-limit-exceed' => 'Regex Fun の{{PLURAL:$1|正規表現処理の最大数 $1}} に達しました。',
 );
 
 /** Macedonian (Македонски)
